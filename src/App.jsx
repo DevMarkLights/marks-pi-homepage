181dc20772865f5219e0e838196bcc7b6eaf8022
Mark Lights <mlights3@gatech.edu>
2025-12-13 15:40:48 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import lights_logo from './assets/lights_logo.png'
+import mark_logo from './assets/mark_logo.png'
+import github_logo from './assets/github.png'
+import linkedin_logo from './assets/InBug-White.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to Mark's pi homepage</h1>
+        <h3>Where would you like to go?</h3>
+        <div className='container'>
+          <div>
+            <img className='lights_logo' src={mark_logo}/>
+          </div>
+          <div>
+            <p>Mark Lights' Portfolio?</p>
+            <a href="https://markjarredlights.com" target="_blank">
+              Yes? Then Click Me!
+            </a>
+          </div>
+        </div>
+        <div className='container'>
+          <div>
+            <img className='lights_logo' src={lights_logo}/>
+          </div>
+          <div>
+            <p>Lights Finance application?</p>
+            <a href="https://marks-pi.com/finance" target="_blank">
+              Yes? Then Click Me!
+            </a>
+          </div>
+        </div>
+        <div className='container'>
+          <div>
+            <img className='lights_logo' src={github_logo}/>
+          </div>
+          <div>
+            <p>Mark Lights' Github?</p>
+            <a href="https://github.com/DevMarkLights" target="_blank">
+              Yes? Then Click Me!
+            </a>
+          </div>
+        </div>
+        <div className='container'>
+          <div>
+            <img className='linkedin_logo' src={linkedin_logo}/>
+          </div>
+          <div>
+            <p>Mark Lights' LinkedIn?</p>
+            <a href="https://linkedin.com/in/mark-lights-1141811bb" target="_blank">
+              Yes? Then Click Me!
+            </a>
+          </div>
+          
+        </div>
+      </div>
+      {/* <h1>Vite + React</h1> */}
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
